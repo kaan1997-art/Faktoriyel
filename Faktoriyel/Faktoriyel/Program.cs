b1aa52d40292fcc3370347d17f0b7eca9f8148b8
kaan1997-art <77634025+kaan1997-art@users.noreply.github.com>
2021-02-07 18:48:09 +0300

    Add files via upload

---
+++ Faktoriyel/Faktoriyel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pozitif bir tam sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = 1;
+            if (sayi < 1 && sayi != 0)
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı giriniz");
+            }
+            else
+            {
+                for (int i = 1; i < sayi; i++)
+                {
+                    faktoriyel += faktoriyel * i;
+                }
+                Console.WriteLine(sayi + " sayısının faktoriyeli:" + faktoriyel);
+            }
+            Console.ReadLine();
+        }
+    }
+}
